5b009dfe96da8241426213e3c30ca5c0cc92939e
yoh-ad <yohad2004@gmail.com>
2025-08-03 15:32:30 +0300

    Add files via upload

---
+++ api_project/api/views.py
@@ -0,0 +1,14 @@
+
+from rest_framework import generics, viewsets
+from rest_framework.permissions import IsAuthenticated
+from .models import Book
+from .serializers import BookSerializer
+
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookViewSet(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+    permission_classes = [IsAuthenticated]
--- api_project/api_project/settings.py
+++ api_project/api_project/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'api',
+    'rest_framework.authtoken',
 ]
 
 MIDDLEWARE = [
