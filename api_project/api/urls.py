cff067c3800d2bedcc52d638b8d10db63ffbc1f9
yoh-ad <yohad2004@gmail.com>
2025-08-03 15:43:12 +0300

    Add files via upload

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,12 +1,7 @@
 
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
-from .views import BookList, BookViewSet
-
-router = DefaultRouter()
-router.register(r'books_all', BookViewSet, basename='book_all')
+from django.urls import path
+from rest_framework.authtoken.views import obtain_auth_token
 
 urlpatterns = [
-    path('books/', BookList.as_view(), name='book-list'),
-    path('', include(router.urls)),
+    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
 ]
---
+++ api_project/settings.py
@@ -0,0 +1,26 @@
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
+    'api',
+]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
+
+SECRET_KEY = 'your-secret-key'
+DEBUG = True
+ROOT_URLCONF = 'api_project.urls'
+ALLOWED_HOSTS = []
