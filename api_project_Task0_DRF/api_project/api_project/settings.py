67a09fe8d20951af1c817b5271bde56332d9eb60
yoh-ad <yohad2004@gmail.com>
2025-08-03 14:53:51 +0300

    Add files via upload

---
+++ api_project_Task0_DRF/api_project/api_project/settings.py
@@ -0,0 +1,13 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    'rest_framework',
+    'api',
+]
+
+# Other default settings omitted for brevity
