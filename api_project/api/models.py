014d40969ad2aaddb78bec5fb75ceaead76fc3da
yoh-ad <yohad2004@gmail.com>
2025-08-03 16:04:08 +0300

    Add files via upload

--- api_project/api/models.py
+++ api_project/api/models.py
@@ -2,8 +2,8 @@
 from django.db import models
 
 class Book(models.Model):
-    title = models.CharField(max_length=100)
-    author = models.CharField(max_length=100)
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
 
     def __str__(self):
         return self.title
--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,7 +1,12 @@
 
-from django.urls import path
-from rest_framework.authtoken.views import obtain_auth_token
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookList, BookViewSet
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
-    path('api-token-auth/', obtain_auth_token, name='api_token_auth'),
+    path('books/', BookList.as_view(), name='book-list'),
+    path('', include(router.urls)),
 ]
