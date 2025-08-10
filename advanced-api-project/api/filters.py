ab9e748f53b311aaf0ed5297497395494d55a0e0
yoh-ad <yohad2004@gmail.com>
2025-08-10 20:22:34 +0300

    Delete advanced-api-project directory

--- advanced-api-project/api/filters.py
+++
@@ -1,9 +0,0 @@
-import django_filters
-from .models import Book
-class BookFilter(django_filters.FilterSet):
-    title = django_filters.CharFilter(field_name='title', lookup_expr='icontains')
-    author = django_filters.CharFilter(field_name='author__name', lookup_expr='icontains')
-    publication_year = django_filters.NumberFilter(field_name='publication_year')
-    class Meta:
-        model = Book
-        fields = ['title','author','publication_year']
