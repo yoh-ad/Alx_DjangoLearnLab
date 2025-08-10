f56d39164b946747ae100e27df92e90d83aadea8
yoh-ad <yohad2004@gmail.com>
2025-08-10 12:41:58 +0300

    Delete advanced-api-project/advanced-api-project directory

--- advanced-api-project/advanced-api-project/api/tests.py
+++
@@ -1,6 +0,0 @@
-# Note: included for completeness; main tests live in api/test_views.py
-from django.test import TestCase
-
-class SmokeTest(TestCase):
-    def test_smoke(self):
-        self.assertTrue(True)
--- advanced-api-project/advanced-api-project/api/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('books/', views.BookListCreateView.as_view(), name='book-list'),
-    path('books/<int:pk>/', views.BookRetrieveUpdateDestroyView.as_view(), name='book-detail'),
-]
